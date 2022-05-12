44e1772d5ad42a8faab5ed926e103f3405987a8a
neuefische <neuefische@Nimas-MacBook-Air.fritz.box>
2022-05-12 14:50:08 +0200

    message/error  added to login

--- client/src/components/functions/createLoginPage.jsx
+++ client/src/components/functions/createLoginPage.jsx
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { app } from "./initializeFirebase";
-import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,updateProfile} from "firebase/auth";
 
 
 const CreateLoginPage = () => {
+
+
   let email = "";
   let password = "";
+
+  const [appMessage,setAppMessage]=useState("");
+  const [messageDisplay,setMessageDisplay]=useState("none")
+  const [messageType,setMessageType]=useState("error")
+
   const auth = getAuth();
   const doLogin = () => {
     email = document.getElementById("email").value;
@@ -17,32 +24,51 @@ const CreateLoginPage = () => {
         // Signed in
         const user = userCredential.user;
         localStorage.setItem("nittoUser", user.uid);
-        alert("You have loged in successfully!");
+        setAppMessage("You have loged in successfully!");
+        setMessageDisplay("block")
+        setMessageType("info")
+        console.log(user)
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        alert(errorMessage);
+        setMessageDisplay("block")
+        setMessageType("error")
+
+        switch(error.code){
+          case "auth/wrong-password":setAppMessage("password is wrong!")
+          break;
+          case "auth/user-not-found":setAppMessage("this email is not registered!")
+          break;
+          case "auth/too-many-requests":setAppMessage("Too many requests! Try again later.")
+          break;
+          default :setAppMessage(errorCode)
+          break;
+        }
       });
   };
 
   const doRegister = () => {
     email = document.getElementById("email").value;
     password = document.getElementById("password").value;
-
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
-        user.displayName = "MohammadHabibi";
-
-        alert("user created with this informations: " + user.email);
+        updateProfile(user,{displayName:"Mohammad Habibi"})
+       
+        setAppMessage("user created with this informations: " + user.email);
+        setMessageDisplay("block")
+        setMessageType("info")
         // ...
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        alert(errorCode + "    " + errorMessage);
+        errorCode=="auth/email-already-in-use"? setAppMessage("this email is registered before!"): setAppMessage(errorCode);
+        setMessageDisplay("block")
+        setMessageType("error")
+
         // ..
       });
   };
@@ -86,7 +112,9 @@ const CreateLoginPage = () => {
           </a>{" "}
           to create your account
         </p>
+        
       </LoginFormDiv>
+      <MessageBox display={messageDisplay}><Message  msgType={messageType}>{appMessage}</Message></MessageBox>
     </>
   );
 };
@@ -133,4 +161,27 @@ const ButtonLogin = styled.button`
 
 const ButtonRegister = styled(ButtonLogin)`
   background: rgb(194, 69, 2);
+`;
+
+const MessageBox=styled.div`
+
+display:${props=> props.display};
+position:absolute;
+left:10px;
+bottom:9%;
+background: white;
+width:auto;
+padding: 0 10px;
+height:50px;
+justify-content:center;
+border-radius:30px 30px 30px 0;
+border:0.5px solid rgb(2, 113, 194);
+box-shadow: rgba(0, 0, 0, 0.26) 0px 2px 5px;
+`;
+
+const Message=styled.p`
+color: ${props=>props.msgType=="error"?"red":"green"} ;
+font-size:1rem;
+font-weight:500;
+text-align:center;
 `;
